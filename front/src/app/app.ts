094c583f63daa443b35d37f955eea92d199add1c
IñakiSP <inakisagra8@gmail.com>
2025-07-05 01:09:49 +0200

    Apartado sobre mi

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from "./components/navbar/navbar";
 import { Hero } from "./components/hero/hero";
+import { AboutMe } from "./components/about-me/about-me";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, AboutMe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
