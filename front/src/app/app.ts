96f5e19a27672a593f87e45f9d315b2910a6333e
IñakiSP <inakisagra8@gmail.com>
2025-07-05 01:39:56 +0200

    Creado el Footer

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -3,10 +3,11 @@ import { Navbar } from "./components/navbar/navbar";
 import { Hero } from "./components/hero/hero";
 import { AboutMe } from "./components/about-me/about-me";
 import { Skills } from "./components/skills/skills";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero, AboutMe, Skills],
+  imports: [Navbar, Hero, AboutMe, Skills, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
