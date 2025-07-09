45432bfbf4ea2a137d368ff60f325fb1e9da5661
IñakiSP <inakisagra8@gmail.com>
2025-07-09 12:09:18 +0200

    Creado el contacto

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -5,10 +5,11 @@ import { AboutMe } from "./components/about-me/about-me";
 import { Skills } from "./components/skills/skills";
 import { Footer } from "./components/footer/footer";
 import { Experience } from "./components/experience/experience";
+import { Contact } from "./components/contact/contact";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero, AboutMe, Skills, Footer, Experience],
+  imports: [Navbar, Hero, AboutMe, Skills, Footer, Experience, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
